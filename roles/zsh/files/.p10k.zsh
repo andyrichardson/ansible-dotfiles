c3a6d05957bd64dc4eabbe39596b5fee45cf9e05
Andy Richardson <EMAIL>
2021-12-15 21:42:00 +0000

    We getting there

--- roles/zsh/files/.p10k.zsh
+++ roles/zsh/files/.p10k.zsh
@@ -208,7 +208,7 @@
   # Current directory background color.
   typeset -g POWERLEVEL9K_DIR_BACKGROUND=4
   # Default current directory foreground color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=0
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=255
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
